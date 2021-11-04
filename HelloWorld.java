9be69c55400666c62d5ef049b683f8183b30ac7c
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-11-04 17:58:07 -0400

    Nov-4-2021_5:58_PM

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+class HelloWorld {
+  public static void main (String[] args){
+    String hello = "Hi mom.";
+    System.out.println(hello);
+  }
+
+  public static String sayHi() {
+    return "Hiya.";
+  }
+
+}
