f348876d947da7cd5200c57b0a4e7222bd273b22
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-11-23 18:48:09 -0500

    Nov-23-2021_6:48_PM

--- BowlingCalculator.java
+++ BowlingCalculator.java
@@ -43,25 +43,30 @@ public class BowlingCalculator {
       consecutiveStrikeCounter++;
       currentFrameIsAStrike = true;
     } else if (firstBall + secondBall == 10) {
-      consecutiveStrikeCounter = 0;
       currentFrameIsASpare = true;
     }
-    // These statements need to appear before the beginning of the return statements.
-    previousFrameWasASpare = currentFrameIsASpare;
-    previousFrameWasAStrike = currentFrameIsAStrike;
     // Simplest case; no marks to account for.
     if (!previousFrameWasASpare && !previousFrameWasAStrike && (firstBall + secondBall != 10)){
       printFrame(frameNumber, totalScore);
-      return;
     }
     if (previousFrameWasASpare) {
       totalScore += firstBall;
       printFrame(frameNumber - 1, totalScore);
-      return;
     }
     if (previousFrameWasAStrike && (firstBall + secondBall != 10)) {
       printFrame(frameNumber - 1, totalScore);
-      return;
+      totalScore += firstBall + secondBall;
+      printFrame(frameNumber, totalScore);
+    }
+    previousFrameWasASpare = currentFrameIsASpare;
+    previousFrameWasAStrike = currentFrameIsAStrike;
+    if (firstBall != 10) {
+      consecutiveStrikeCounter = 0;
+    }
+    if ((consecutiveStrikeCounter == 2) && (firstBall + secondBall != 10)) {
+      // printFrame(frameNumber - 2, totalScore);
+      // totalScore += firstBall + secondBall;
+      // printFrame(frameNumber, totalScore);
     }
 
 /*     if (previousFrameWasAStrike && firstBall != 10) {
